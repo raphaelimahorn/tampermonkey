1f385327e6511f2756551e5ef897d8b601b8c344
Raphael Imahorn <raphael.imahorn@digitecgalaxus.ch>
2023-02-03 10:51:12 +0100

    Fix jira buttons script
    Signed-off-by: Raphael Imahorn <raphael.imahorn@digitecgalaxus.ch>

--- jira/Changelog.md
+++ jira/Changelog.md
@@ -10,6 +10,7 @@
 * 1.0.2 Update default url to cloud version
 
 ## issue_buttons.user.js
+* 2.2.2. Fix issue, that path was not found on contextmenu event
 * 2.2.1. Export branch name in lower key
 * 2.2.0. Add support for navigation between pages
 * 2.1.0. Add support for backlog page
--- jira/issue_buttons.user.js
+++ jira/issue_buttons.user.js
@@ -1,7 +1,7 @@
 ﻿// ==UserScript==
 // @name         Jira Issue Buttons
 // @namespace    http://tampermonkey.net/
-// @version      2.2.1
+// @version      2.2.2
 // @updateURL    https://raw.githubusercontent.com/raphaelimahorn/tampermonkey/main/jira/issue_buttons.user.js
 // @downloadURL  https://raw.githubusercontent.com/raphaelimahorn/tampermonkey/main/jira/issue_buttons.user.js
 // @description  adds some functionality to jira issues
@@ -153,7 +153,7 @@
             return;
         }
 
-        const card = getIssueCardOrNone(contextEvent.path);
+        const card = getIssueCardOrNone(contextEvent.composedPath());
         if (!card) {
             return;
         }
