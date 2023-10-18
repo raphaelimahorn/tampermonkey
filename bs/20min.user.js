eecddfa5e799497ec67fd442d504de7da0928c19
raphaelimahorn <r.imahorn@gmail.com>
2023-10-18 07:28:46 +0200

    fix 20min userscript by introducing an eventlistener
    Signed-off-by: Raphael Imahorn <raphael.imahorn@digitecgalaxus.ch>

--- bs/20min.user.js
+++ bs/20min.user.js
@@ -1,21 +1,23 @@
 // ==UserScript==
 // @name         Filter 20 Min
 // @namespace    http://tampermonkey.net/
-// @version      0.1
+// @version      0.2
 // @description  Filters away bad content from 20 min
 // @author       raphael.imahorn
 // @match        https://www.20min.ch/*
 // @grant        none
 // ==/UserScript==
 
-(function() {
+(function () {
     'use strict';
 
-    var sheet = document.styleSheets[0];
-    var style =
+    document.addEventListener("DOMContentLoaded", () => {
+        const sheet = document.styleSheets[0];
+        const style = 
 `p, span, h1, h2, h3, h4, dt, dd, dl, img, button, figure {
     filter: blur(.25em);
 }`;
 
-    sheet.insertRule(style);
+        sheet.insertRule(style);
+    });
 })();
